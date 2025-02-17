f3e4762f4793f13144823f9f786b95ef1334081a
archie <38737795+archizzl@users.noreply.github.com>
2025-02-17 17:48:05 -0500

    ok

--- fart.py
+++ fart.py
@@ -2,7 +2,7 @@ import os
 
 def create_directory(directory, caption):
     with open(os.path.join(directory, 'index.html'), 'w') as f:
-        f.write('<html><head><style>* { color: black; text-decoration: none; }</style></head><body><h3>' + caption + '</h3><ol>')
+        f.write('<html><head><style>* { color: black; text-decoration: none; } li:hover { text-decoration: underline; cursor: pointer; }</style></head><body><h3>' + caption + '</h3><ol>')
         for filename in sorted(os.listdir(directory)):
             if filename.endswith('.txt'):
                 f.write(f'<li><a target="_blank" href="{filename}">{filename[:-4]}</a></li>')
